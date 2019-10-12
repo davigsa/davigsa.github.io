78decc4df5105f5bdc97e242a6d48b707ea2bc7e
Davi Goulart <davi.goulartsa@gmail.com>
2019-10-12 09:39:53 -0300

    some changes

---
+++ data.js
@@ -0,0 +1,3 @@
+const text = {
+    header = ['About me', 'Projects', 'Contact']
+};
--- index.js
+++ index.js
@@ -3,3 +3,21 @@ window.scrollBy({
     left: 0, 
     behavior: 'smooth'
   });
+
+const text = document.querySelector('.text-trade');
+const about = document.querySelector('.about-me');
+const skills = document.querySelector('.skills');
+const green = document.querySelector('.green');
+const purple = document.querySelector('.purple');
+
+purple.addEventListener('click', event => {
+    about.style.display = 'none'
+    skills.style.display = 'block';
+});
+
+green.addEventListener('click', event => {
+  about.style.display = 'block'
+  skills.style.display = 'none';
+});
+
+		
